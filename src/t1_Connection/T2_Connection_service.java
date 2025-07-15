e53d5f247f109ee5e4849011cae34b88d33a893f
eeeool <engo101010@gmail.com>
2025-07-15 17:26:15 +0900

    20250715_1725 database초기파일

---
+++ src/t1_Connection/T2_Connection_service.java
@@ -0,0 +1,11 @@
+package t1_Connection;
+
+public class T2_Connection_service {
+	public static void main(String[] args) {
+		System.out.println("1. main 화면 실행");
+//		T2_ConnectionDAO new T2_ConnectionDAO();
+		
+//		T2_ConnectionService service = new T2_ConnectionService();
+		
+	}
+}
