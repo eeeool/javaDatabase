44f6da11f05eecc328ca70fd708a422408ccf73c
eeeool <engo101010@gmail.com>
2025-07-15 17:52:15 +0900

    20250715_1752 run수정, test삭제

--- src/t5_Hoewon/HoewonRun.java
+++ src/t5_Hoewon/HoewonRun.java
@@ -38,6 +38,8 @@ public class HoewonRun {
 		System.out.println("\n작업을 종료합니다!.......");
 		System.out.println("\n작업을 종료합니다!!.......");
 		System.out.println("\n작업을 종료합니다!!!.......");
+		System.out.println("\n작업을 종료합니다!!!!.......");
+		System.out.println("\n작업을 종료합니다!!!!!.......");
 		
 		scanner.close();
 	}
